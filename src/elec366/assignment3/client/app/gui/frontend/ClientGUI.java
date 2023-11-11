f0d3750aabbb62203e7133e88997c3b73df499e4
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-11-11 15:49:38 +0800

    Client GUI: Skeleton

---
+++ src/elec366/assignment3/client/app/gui/frontend/ClientGUI.java
@@ -0,0 +1,81 @@
+package elec366.assignment3.client.app.gui.frontend;
+
+public class ClientGUI implements IClientGUI {
+	
+	public ClientGUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onConnectionButton(Runnable callback) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSendButton(Runnable callback) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setState(State state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayDialog(String dialogTitle, String displayedMessage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setApplicationTitle(String applicationTitle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearChat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void appendChat(String appendedLine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOnlinePlayers(String[] onlinePlayerNames) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRecepient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getChat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
