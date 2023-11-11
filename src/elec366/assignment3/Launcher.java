704c986ecf8a9072f8589407795f5ae83a6d3d66
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-11-12 01:55:42 +0800

    Server GUI: Imported code

--- src/elec366/assignment3/Launcher.java
+++ src/elec366/assignment3/Launcher.java
@@ -1,20 +1,14 @@
 package elec366.assignment3;
 
-import elec366.assignment3.client.app.gui.GraphicalClientLauncher;
-import elec366.assignment3.client.app.type.ConnectionInformation;
-import elec366.assignment3.server.app.console.ConsoleServer;
+import elec366.assignment3.server.app.gui.frontend.IServerGUI;
+import elec366.assignment3.server.app.gui.frontend.ServerGUI;
 
 public class Launcher {
 
 	public static void main(String args[]) throws InterruptedException {
 		
-		if(args.length == 1) {
-			(new ConsoleServer(14567)).start();  
-		}
-		else {
-			(new GraphicalClientLauncher(new ConnectionInformation("localhost", 14567))).run();
-		}
-		
+		IServerGUI serverGUI = new ServerGUI(); 
+		serverGUI.showUI();
 	}
 
 }
--- src/elec366/assignment3/server/app/gui/frontend/ServerGUI.java
+++ src/elec366/assignment3/server/app/gui/frontend/ServerGUI.java
@@ -1,9 +1,43 @@
 package elec366.assignment3.server.app.gui.frontend;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 public class ServerGUI implements IServerGUI {
 
 	public ServerGUI() {
-		// TODO Auto-generated constructor stub
+		JFrame frame = new JFrame();
+		frame.setBounds(100, 100, 500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Chatting Server");
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+		//Not connected
+		//I was thinking we can add an if else statement? 
+		//I just wasn't super clear on how to set it up...
+		JLabel labelNotConnected = new JLabel("Text Color: Red"); 
+		labelNotConnected.setForeground(Color.red);
+		labelNotConnected.setBounds(100, 50, 300, 30); 
+		labelNotConnected.setFont(new Font("Times", Font.BOLD, 14));
+		labelNotConnected.setHorizontalAlignment(SwingConstants.LEFT);
+		labelNotConnected.setVerticalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(labelNotConnected);
+		labelNotConnected.setText("Connection Status: Not Connected"); 
+		
+		//Need to set up a counter of clients to work with the connection box
+		
+		JLabel labelConnected = new JLabel("Text Color: Blue"); 
+		labelConnected.setForeground(Color.blue);
+		labelConnected.setBounds(150, 50, 300, 30); 
+		labelConnected.setFont(new Font("Times", Font.BOLD, 14));
+		labelConnected.setHorizontalAlignment(SwingConstants.LEFT);
+		labelConnected.setVerticalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(labelConnected);
+		labelConnected.setText("Number Clients Connected");
 	}
 
 	@Override
