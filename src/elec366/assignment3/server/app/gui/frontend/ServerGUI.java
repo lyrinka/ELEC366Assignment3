11dc2e3269668d9af2f7b9123b57e26b39c1e63c
Lyrinka Hyacinthoides <47382398+lyrinka@users.noreply.github.com>
2023-11-12 01:53:28 +0800

    Server GUI: Skeleton

---
+++ src/elec366/assignment3/server/app/gui/frontend/ServerGUI.java
@@ -0,0 +1,33 @@
+package elec366.assignment3.server.app.gui.frontend;
+
+public class ServerGUI implements IServerGUI {
+
+	public ServerGUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void showUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setState(State state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOnlinePlayers(String[] players) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void appendLog(String appendedLine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
