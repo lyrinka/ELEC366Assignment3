91fe80dac55b2afec7c105e72b78ab983746f95e
RobinP457 <98273238+RobinP457@users.noreply.github.com>
2023-11-13 22:11:57 -0500

    Server: Improved server resources

--- src/elec366/assignment3/server/ServerResources.java
+++ src/elec366/assignment3/server/ServerResources.java
@@ -4,45 +4,45 @@ public class ServerResources {
 
 	public static class LOGIN {
 		
-		public static String CONSOLE_PREFIX = "&e%s lost connection: "; 
-		public static String NAME_CONFLICT = "&c&lUser with name &e%s &c&l is already online!"; 
-		public static String NAME_TOO_SHORT = "&c&lYour username is too short (at least %d characters)."; 
-		public static String NAME_TOO_LONG = "&c&lYour username is too long (at most %d characters).";
-		public static String INVALID_CHARACTER = "&c&lYour username contains invalid characters. &aAllowed: letters, numbers, underscore."; 
+		public static String CONSOLE_PREFIX = "&4%s lost connection: "; 
+		public static String NAME_CONFLICT = "&4&lUser with name &8%s &4&l is already online!"; 
+		public static String NAME_TOO_SHORT = "&4&lYour username is too short (at least %d characters)."; 
+		public static String NAME_TOO_LONG = "&4&lYour username is too long (at most %d characters).";
+		public static String INVALID_CHARACTER = "&4&lYour username contains invalid characters. &2Allowed: letters, numbers, underscore."; 
 		
 	}
 	
 	public static class JOIN {
 		
-		public static String MESSAGE = "&e%s has connected.";
-		public static String WELCOME1 = "&a&lWelcome! There are %d users online."; 
+		public static String MESSAGE = "&8%s has connected.";
+		public static String WELCOME1 = "&2&lWelcome! There are %d users online."; 
 		public static String WELCOME2 = "Current time: %s"; 
-		public static String WELCOME3 = "&aOnline: &e%s"; 
+		public static String WELCOME3 = "&2Online: &8%s"; 
 		
 	}
 	
 	public static class QUIT {
 		
-		public static String MESSAGE = "&e%s has disconnected.";
+		public static String MESSAGE = "&8%s has disconnected.";
 		
 	}
 	
 	public static class CHAT {
 		
-		public static String OVERSIZED_LOG = "&cUser %s sent oversized message (length=%d)."; 
-		public static String OVERSIZED = "&cYour message is too long!"; 
+		public static String OVERSIZED_LOG = "&4User %s sent oversized message (length=%d)."; 
+		public static String OVERSIZED = "&4Your message is too long!"; 
 		
-		public static String FORMAT = "&7<&b%s&7> &r%s"; 
+		public static String FORMAT = "&8<&3%s&7> &r%s"; 
 		
 	}
 	
 	public static class COMMAND {
 		
-		public static String ISSUE_LOG = "&7%s issued server command: %s"; 
+		public static String ISSUE_LOG = "&8%s issued server command: %s"; 
 		
-		public static String UNKNOWN = "&7Unknown command. Try /help."; 
-		public static String EXCEPTION = "&c&lAn internal error has occured. Please contact server administorators.";
-		public static String MALFORMAT = "&7Unrecognized command format. Try /help %s."; 
+		public static String UNKNOWN = "&8Unknown command. Try /help."; 
+		public static String EXCEPTION = "&4&lAn internal error has occured. Please contact server administorators.";
+		public static String MALFORMAT = "&8Unrecognized command format. Try /help %s."; 
 		
 	}
 	
@@ -50,9 +50,9 @@ public class ServerResources {
 		
 		public static String HELP = "General help topic (todo)"; 
 		
-		public static String TOPIC_ON_COMMAND1 = "&fCommand &b&l%s&f: &r%s"; 
-		public static String TOPIC_ON_COMMAND2 = "&f .. &r%s"; 
-		public static String TOPIC_DEFAULT = "&fNo help for command &b&l%s&f."; 
+		public static String TOPIC_ON_COMMAND1 = "&8Command &3&l%s&8: &r%s"; 
+		public static String TOPIC_ON_COMMAND2 = "&8 .. &r%s"; 
+		public static String TOPIC_DEFAULT = "&8No help for command &3&l%s&8."; 
 		
 	}
 	
@@ -70,8 +70,8 @@ public class ServerResources {
 		
 		public static String HELP = "Usage: /list"; 
 		
-		public static String MESSAGE1 = "&aThere are %d users online: "; 
-		public static String MESSAGE2 = "&e%s"; 
+		public static String MESSAGE1 = "&2There are %d users online: "; 
+		public static String MESSAGE2 = "&9%s"; 
 		
 	}
 	
@@ -81,9 +81,9 @@ public class ServerResources {
 		
 		public static String OFFLINE = "The player is not online."; 
 		public static String SELF = "The player is not online."; 
-		public static String MESSAGE = "&f[%s&f -> %s&f]&r %s"; 
-		public static String FORMAT_OTHERS = "&b%s"; 
-		public static String FORMAT_YOU = "&cYou"; 
+		public static String MESSAGE = "&8[%s&8 -> %s&8]&r %s"; 
+		public static String FORMAT_OTHERS = "&1%s"; 
+		public static String FORMAT_YOU = "&4You"; 
 		
 	}
 	
